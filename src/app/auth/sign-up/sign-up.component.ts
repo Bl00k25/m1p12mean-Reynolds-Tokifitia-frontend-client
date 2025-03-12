e07c4510c326e2b78bba8d47114faf47c9c076be
Bl00k25 <koronosensei51@gmail.com>
2025-03-12 23:08:15 +0300

    Toky:features/ Created the sign in and sign up pages at /#/signin and /#/signup

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,34 @@
+// sign-up.component.ts
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+}
